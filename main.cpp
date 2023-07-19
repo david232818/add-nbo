5845aaab1cc9f970f27dd8522ddfa94228d8570e
david232818 <david232818@gmail.com>
2023-07-19 10:04:52 +0900

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include "add.h"
+
+int main(int argc, char *argv[])
+{
+  uint32_t n1, n2, sum;
+  FILE *rfp1, *rfp2;
+  
+  if (argc != 3) {
+    fputs("add-nbo <file1> <file2>\n", stderr);
+    return -1;
+  }
+
+  if ((rfp1 = fopen(argv[1], "r")) == NULL)
+    goto FILE_OPEN_ERR;
+
+  if ((rfp2 = fopen(argv[2], "r")) == NULL)
+    goto FILE_OPEN_ERR;
+
+  if (fread(&n1, sizeof(n1), 1, rfp1) != 1)
+    goto FILE_READ_ERR;
+  
+  if (fread(&n2, sizeof(n2), 1, rfp2) != 1)
+    goto FILE_READ_ERR;
+
+  sum = add(n1, n2, true);
+  printf("%d(%x) + %d(%x) = %d(%x)\n", ntohl(n1), ntohl(n1),
+	 ntohl(n2), ntohl(n2), sum, sum);
+  return 0;
+
+ FILE_OPEN_ERR:
+  perror("fopen(): ");
+  return -1;
+
+ FILE_READ_ERR:
+  perror("fread(): ");
+  return -1;
+}
+  
